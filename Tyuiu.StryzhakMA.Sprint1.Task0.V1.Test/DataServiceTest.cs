3feec3ed10ac93846da9b4fc80e90485192a4541
Стрыжак Мария < @LAPTOP-STRYZHAK>
2026-09-20 23:16:17 +0500

    Add project files.

---
+++ Tyuiu.StryzhakMA.Sprint1.Task0.V1.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.StryzhakMA.Sprint1.Task0.V1.Lib;
+
+namespace Tyuiu.StryzhakMA.Sprint1.Task0.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(-6, res);
+        }
+    }
+}
